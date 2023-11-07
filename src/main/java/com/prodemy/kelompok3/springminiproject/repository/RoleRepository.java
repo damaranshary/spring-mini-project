7b01c3f538eb29eb7ba47f9f565c4ede16a954f4
Damar Anshary <damar.galihanshary7@gmail.com>
2023-11-07 23:28:16 +0700

    add authority configuration (role repo)

--- src/main/java/com/prodemy/kelompok3/springminiproject/repository/RoleRepository.java
+++ src/main/java/com/prodemy/kelompok3/springminiproject/repository/RoleRepository.java
@@ -4,7 +4,9 @@ package com.prodemy.kelompok3.springminiproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prodemy.kelompok3.springminiproject.entity.Role;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }

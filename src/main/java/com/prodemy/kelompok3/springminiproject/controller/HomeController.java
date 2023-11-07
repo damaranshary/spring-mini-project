d4495b091d7c8092eee59ca1d3c9f23371a3dde2
Damar Anshary <damar.galihanshary7@gmail.com>
2023-11-07 23:26:31 +0700

    fixing default html pages

---
+++ src/main/java/com/prodemy/kelompok3/springminiproject/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.prodemy.kelompok3.springminiproject.controller;
+
+import com.prodemy.kelompok3.springminiproject.entity.User;
+import com.prodemy.kelompok3.springminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        User user = userService.getCurrentUser();
+
+        String role = user.getRoles().get(0).getName();
+
+        model.addAttribute("user", user);
+
+        if (role.equals("ROLE_ADMIN")){
+            return "indexAdmin";
+        }
+
+        return "indexUser";
+    }
+}
